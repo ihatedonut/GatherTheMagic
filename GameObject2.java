9b32a6194a2b73f5778faecca2e27b11bdcdf00c
MikeSky424 <105068731+MikeSky424@users.noreply.github.com>
2022-05-11 12:43:13 -0400

    Add files via upload
    adds an object class, which is checking life

---
+++ GameObject2.java
@@ -0,0 +1,19 @@
+
+public class GameObject2 {
+
+	public static void main(String[] args)
+	{
+		Player p1 = new Player("none");
+		Player p2 = new Player("none");
+		
+				
+		while(p1.getLife()>0 && p2.getLife()>0)
+		{
+			
+		}
+		
+		
+		
+	}
+
+}
